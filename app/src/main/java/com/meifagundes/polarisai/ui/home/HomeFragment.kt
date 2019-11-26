5a95eb7ebaea2a895841c083fff41fd3593aaf9a
Mei Fagundes <MeiFagundes@outlook.com>
2019-11-26 10:22:09 -0300

    Add chat view resources

--- app/src/main/java/com/meifagundes/polarisai/ui/home/HomeFragment.kt
+++ app/src/main/java/com/meifagundes/polarisai/ui/home/HomeFragment.kt
@@ -5,39 +5,53 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.meifagundes.polarisai.MessageListAdapter
 import com.meifagundes.polarisai.R
+import com.meifagundes.polarisai.model.Message
 import kotlinx.android.synthetic.main.fragment_home.*
-import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.uiThread
-import org.json.JSONObject
-import java.net.URL
+import java.util.*
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private val messages = listOf(
+        Message("Who are you?", Calendar.getInstance(), 1),
+        Message("I'm the brightest star in the constellation of Ursa Minor, Polaris!", Calendar.getInstance(), 2)
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        return root
+        //homeViewModel =
+        //        ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        println("z")
-        this.sendBtn.setOnClickListener {
-            setResponse()
+        //this.sendBtn.setOnClickListener {
+        //    setResponse()
+        //}
+
+        message_list.apply {
+            // set a LinearLayoutManager to handle Android
+            // RecyclerView behavior
+            layoutManager = LinearLayoutManager(activity)
+            // set the custom adapter to the RecyclerView
+            adapter = MessageListAdapter(messages)
         }
+
+        /*println(activity == null)
+        messageRecycler = message_list as RecyclerView
+        messageAdapter = activity?.let { MessageListAdapter(it, messages) }
+        messageRecycler!!.layoutManager = LinearLayoutManager(activity)*/
+
     }
 
-    private fun setResponse(){
+    /*private fun setResponse(){
         responseTxt.text = "Awaiting response..."
         doAsync {
             val response = URL("https://polarisai.azurewebsites.net/query/${queryTxt.text}").readText()
@@ -46,5 +60,5 @@ class HomeFragment : Fragment() {
                 responseTxt.text = json["response"].toString()
             }
         }
-    }
+    }*/
 }
